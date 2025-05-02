363858f7840a5c8a4e0b6d5862984e6cc1d304f4
Anthony Martinez <retriblue7@gmail.com>
2025-05-02 23:32:44 +0000

    Implemented user registration (requirement 1)

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,12 +1,37 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.service.AccountService;
+import com.example.service.MessageService;
+import com.example.entity.Account;
+import com.example.entity.Message;
+import com.example.exception.*;
 
-/**
- * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
- * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
- * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
- * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
- */
+
+@RestController
 public class SocialMediaController {
+    private AccountService accountService;
+    private MessageService messageService;
 
+    @Autowired
+    public SocialMediaController(AccountService accountService, MessageService messageService) {
+        this.accountService = accountService;
+        this.messageService = messageService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<Account> registerUser(@RequestBody Account account) throws DuplicateUsernameException, PasswordTooShortException, BlankUsernameException {
+        return ResponseEntity.status(HttpStatus.OK).body(accountService.registerUser(account));
+    }
+
+    @ExceptionHandler(DuplicateUsernameException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public void conflictStatus() {}
+
+    @ExceptionHandler({BlankUsernameException.class, PasswordTooShortException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void clientErrorStatus() {}
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,33 @@
 package com.example.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import com.example.repository.AccountRepository;
+import com.example.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.example.exception.BlankUsernameException;
+import com.example.exception.DuplicateUsernameException;
+import com.example.exception.PasswordTooShortException;
+
+
+@Service
 public class AccountService {
+    
+    private AccountRepository accountRepo;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepo) {
+        this.accountRepo = accountRepo;
+    }
+
+    public Account registerUser(Account account) throws BlankUsernameException, PasswordTooShortException, DuplicateUsernameException {
+        if (account.getUsername().isEmpty())
+            throw new BlankUsernameException();
+        else if (account.getPassword().length() < 4)
+            throw new PasswordTooShortException();
+        else if (accountRepo.findByUsername(account.getUsername()).isPresent())
+            throw new DuplicateUsernameException();
+        else
+            return accountRepo.save(account);
+    }
 }
